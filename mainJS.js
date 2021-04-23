7e0120db0d5d93faf0f6dc4e51ad6296a725a437
barweiner2003 <55712179+barweiner2003@users.noreply.github.com>
2021-04-22 19:06:13 -0700

    Add files via upload

---
+++ mainJS.js
@@ -0,0 +1,633 @@
+$(document).ready(function ()
+{
+	$("#button11").mouseenter(function ()
+	{
+		var imageUrl = "img/11.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button12").mouseenter(function ()
+	{
+		var imageUrl = "img/12.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button13").mouseenter(function ()
+	{
+		var imageUrl = "img/13.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button14").mouseenter(function ()
+	{
+		var imageUrl = "img/14.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button15").mouseenter(function ()
+	{
+		var imageUrl = "img/15.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button16").mouseenter(function ()
+	{
+		var imageUrl = "img/16.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button17").mouseenter(function ()
+	{
+		var imageUrl = "img/17.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button18").mouseenter(function ()
+	{
+		var imageUrl = "img/18.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button19").mouseenter(function ()
+	{
+		var imageUrl = "img/19.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button20").mouseenter(function ()
+	{
+		var imageUrl = "img/20.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button21").mouseenter(function ()
+	{
+		var imageUrl = "img/21.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button22").mouseenter(function ()
+	{
+		var imageUrl = "img/22.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button23").mouseenter(function ()
+	{
+		var imageUrl = "img/23.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button24").mouseenter(function ()
+	{
+		var imageUrl = "img/24.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button25").mouseenter(function ()
+	{
+		var imageUrl = "img/25.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button26").mouseenter(function ()
+	{
+		var imageUrl = "img/26.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button27").mouseenter(function ()
+	{
+		var imageUrl = "img/27.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button28").mouseenter(function ()
+	{
+		var imageUrl = "img/28.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button29").mouseenter(function ()
+	{
+		var imageUrl = "img/29.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button30").mouseenter(function ()
+	{
+		var imageUrl = "img/30.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button31").mouseenter(function ()
+	{
+		var imageUrl = "img/31.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button32").mouseenter(function ()
+	{
+		var imageUrl = "img/32.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button33").mouseenter(function ()
+	{
+		var imageUrl = "img/33.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button34").mouseenter(function ()
+	{
+		var imageUrl = "img/34.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button35").mouseenter(function ()
+	{
+		var imageUrl = "img/35.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button36").mouseenter(function ()
+	{
+		var imageUrl = "img/36.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button37").mouseenter(function ()
+	{
+		var imageUrl = "img/37.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button38").mouseenter(function ()
+	{
+		var imageUrl = "img/38.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button39").mouseenter(function ()
+	{
+		var imageUrl = "img/39.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button40").mouseenter(function ()
+	{
+		var imageUrl = "img/40.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button41").mouseenter(function ()
+	{
+		var imageUrl = "img/41.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button42").mouseenter(function ()
+	{
+		var imageUrl = "img/42.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button43").mouseenter(function ()
+	{
+		var imageUrl = "img/43.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button44").mouseenter(function ()
+	{
+		var imageUrl = "img/44.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button45").mouseenter(function ()
+	{
+		var imageUrl = "img/45.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button46").mouseenter(function ()
+	{
+		var imageUrl = "img/46.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button47").mouseenter(function ()
+	{
+		var imageUrl = "img/47.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button48").mouseenter(function ()
+	{
+		var imageUrl = "img/48.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button49").mouseenter(function ()
+	{
+		var imageUrl = "img/49.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button50").mouseenter(function ()
+	{
+		var imageUrl = "img/50.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button51").mouseenter(function ()
+	{
+		var imageUrl = "img/51.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button52").mouseenter(function ()
+	{
+		var imageUrl = "img/52.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button53").mouseenter(function ()
+	{
+		var imageUrl = "img/53.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button54").mouseenter(function ()
+	{
+		var imageUrl = "img/54.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button55").mouseenter(function ()
+	{
+		var imageUrl = "img/55.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button56").mouseenter(function ()
+	{
+		var imageUrl = "img/56.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+	$("#button57").mouseenter(function ()
+	{
+		var imageUrl = "img/57.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button58").mouseenter(function ()
+	{
+		var imageUrl = "img/58.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button59").mouseenter(function ()
+	{
+		var imageUrl = "img/59.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button60").mouseenter(function ()
+	{
+		var imageUrl = "img/60.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button61").mouseenter(function ()
+	{
+		var imageUrl = "img/61.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button62").mouseenter(function ()
+	{
+		var imageUrl = "img/62.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button63").mouseenter(function ()
+	{
+		var imageUrl = "img/63.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button64").mouseenter(function ()
+	{
+		var imageUrl = "img/64.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button65").mouseenter(function ()
+	{
+		var imageUrl = "img/65.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button66").mouseenter(function ()
+	{
+		var imageUrl = "img/66.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button67").mouseenter(function ()
+	{
+		var imageUrl = "img/67.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button68").mouseenter(function ()
+	{
+		var imageUrl = "img/68.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button69").mouseenter(function ()
+	{
+		var imageUrl = "img/69.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button70").mouseenter(function ()
+	{
+		var imageUrl = "img/70.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button71").mouseenter(function ()
+	{
+		var imageUrl = "img/71.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button72").mouseenter(function ()
+	{
+		var imageUrl = "img/72.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button73").mouseenter(function ()
+	{
+		var imageUrl = "img/73.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button74").mouseenter(function ()
+	{
+		var imageUrl = "img/74.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button75").mouseenter(function ()
+	{
+		var imageUrl = "img/75.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button76").mouseenter(function ()
+	{
+		var imageUrl = "img/56.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button77").mouseenter(function ()
+	{
+		var imageUrl = "img/77.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button78").mouseenter(function ()
+	{
+		var imageUrl = "img/78.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button79").mouseenter(function ()
+	{
+		var imageUrl = "img/79.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button80").mouseenter(function ()
+	{
+		var imageUrl = "img/80.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button81").mouseenter(function ()
+	{
+		var imageUrl = "img/81.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button82").mouseenter(function ()
+	{
+		var imageUrl = "img/82.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button83").mouseenter(function ()
+	{
+		var imageUrl = "img/83.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button84").mouseenter(function ()
+	{
+		var imageUrl = "img/84.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button85").mouseenter(function ()
+	{
+		var imageUrl = "img/85.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button86").mouseenter(function ()
+	{
+		var imageUrl = "img/86.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button87").mouseenter(function ()
+	{
+		var imageUrl = "img/87.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+    $("#button88").mouseenter(function ()
+	{
+		var imageUrl = "img/88.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+    $("#button89").mouseenter(function ()
+	{
+		var imageUrl = "img/89.png";
+		$(".graph").css("background-image", "url(" + imageUrl + ")");
+
+    })
+
+
+
+
+
+
+
+}
+
+
+
+
+
+)
+
